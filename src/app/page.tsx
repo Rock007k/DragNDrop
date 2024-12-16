cdb8dd30461f7a90c033babbe249b9d4571a3a6a
Rock007k <gautammaheshwari135790@gmail.com>
2024-12-16 19:53:07 +0530

    working data from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
+'use client';
+
 import React from 'react';
-import LeftComponent from './components/LeftComponent';
-import RightComponent from './components/RightComponent';
 import DragDropCanvas from './components/DragDropCanvas';
 import NodeList from './components/NodeList';
 
 const Home: React.FC = () => {
+  const handleDragStart = (event: React.DragEvent, nodeType: string) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
 
   return (
     <div className="flex h-screen">
-      <div className="flex-[8]">
    
+      <div className="flex-[8] p-4">
         <DragDropCanvas />
       </div>
-     
-      <div className="flex-[2]">
-      
-        <NodeList/>
+
+      <div className="flex-[2] p-4 bg-gray-200">
+        <NodeList onDragStart={handleDragStart} />
       </div>
     </div>
   );
